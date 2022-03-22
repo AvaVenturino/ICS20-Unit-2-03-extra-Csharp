7c5171bb38f16c8b2cb2867dedaea5780e2b66be
Ava <99355031+AvaVenturino@users.noreply.github.com>
2022-03-22 21:56:25 +0000

    wrote code

--- main.cs
+++ main.cs
@@ -1,7 +1,30 @@
+// Created by: Mr Coxall
+// Created on: Sep 2020
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function accepts user input
+        int streetNumber;
+        string streetName;
+
+        Console.WriteLine("This program gets a user's Street Number and Street Name.");
+        Console.WriteLine("");
+      
+        Console.Write("Enter your Street Number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your Street Name: ");
+        streetName = Console.ReadLine();
+
+
+        Console.WriteLine("");
+        Console.WriteLine("Your address is: " + streetNumber + " " + streetName + ".");
+
+        Console.WriteLine("\nDone.");
+    }
 }
